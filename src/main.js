6492d5d867a52956fcd432e6fc9707d8ab21b447
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2026-03-31 13:26:32 +0300

    Исправлено отображение видео на iOS (iPhone)

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
-import './css/index.css'
-import './css/header.css'
-import './css/hero.css'
-import './css/about.css'
-import './css/advantages.css'
-import './css/prices.css'
-import './css/contact.css'
-import './css/footer.css'
+import "./css/index.css";
+import "./css/header.css";
+import "./css/hero.css";
+import "./css/about.css";
+import "./css/advantages.css";
+import "./css/prices.css";
+import "./css/contact.css";
+import "./css/footer.css";
 
-import { initParallax } from './js/parallax.js'
-import { initNavigation, initTabs, initForm } from './js/navigation.js'
+import { initParallax } from "./js/parallax.js";
+import { initNavigation, initTabs, initForm } from "./js/navigation.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  initNavigation()
-  initTabs()
-  initForm()
+document.addEventListener("DOMContentLoaded", () => {
+  initNavigation();
+  initTabs();
+  initForm();
 
-  initParallax() // Запускаем параллакс принудительно, игнорируя системные настройки (поскольку пользователь явно тестирует эффект)
+  initParallax();
 
-  const video = document.querySelector('.hero__video')
+  const video = document.querySelector(".hero__video");
   if (video) {
-    video.playbackRate = 1.6 
+    video.playbackRate = 1.6;
+
+    // Принудительный запуск видео на iOS
+    const playPromise = video.play();
+    if (playPromise !== undefined) {
+      playPromise.catch(() => {
+        // Автозапуск заблокирован, пробуем при первом взаимодействии
+        const enableVideo = () => {
+          video.play().catch(() => {});
+          document.removeEventListener("touchstart", enableVideo);
+          document.removeEventListener("click", enableVideo);
+        };
+        document.addEventListener("touchstart", enableVideo, { passive: true });
+        document.addEventListener("click", enableVideo, { once: true });
+      });
+    }
   }
-})
+});
